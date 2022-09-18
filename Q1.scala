c8ae545eb0a51456d001010f2df1143d0b19a324
BhanukaRajk <bhanukayar@gmail.com>
2022-09-19 00:59:28 +0530

    Q1 completed 2.0

--- Q1.scala
+++ Q1.scala
@@ -1,45 +1,61 @@
 case class Point(var x: Int, var y: Int) {
+
+    // Question 1.a addition
     def +(that: Point): Point = {
        return Point(this.x + that.x, this.y + that.y)
     }
 
+    // Question 1.b change point with given value
     def move(dx: Int, dy: Int) = {
         this.x = this.x + dx
         this.y = this.y + dy
     }
 
+    // Question 1.c get the distance between defined point and given point
     def distance(x: Int, y: Int): Double = {
         return scala.math.sqrt(math.pow(this.x - x, 2) + math.pow(this.y - y, 2))
     }
 
+    // Question 1.d inverse point
     def invert() = {
         var TEMP = this.y
         this.y = this.x
         this.x = TEMP
     }
+
 }
 
 object Q1 {
-    def main(args: Array[String]):Unit = {
-        var num1 = Point(3, 5)
-        var num2 = Point(1, 4)
+    def main(args: Array[String]) = {
+
+        // Pre defined two points
+        var P1 = Point(3, 5)
+        var P2 = Point(1, 4)
     
-        println(num1 + num2)
+        // Adding two points
+        println("\nNumber1 + Number2 = " + (P1 + P2))
 
-        num1.move(2, 4)
-        println(num1)
+        // Move point 1
+        P1.move(2, 4)
+        println("\nNew point of P1 = " + P1)
 
-        num2.move(3, 8)
-        println(num2)
+        // Move point 2
+        P2.move(3, 8)
+        println("New point of P2 = " + P2)
 
-        println(num1.distance(2, 3))
-        println(num2.distance(4, 5))
+        // Distance calculation
+        println("\nDistance from P1 = " + P1.distance(2, 3))
+        println("Distance from P2 = " + P2.distance(4, 5))
 
-        num1.invert()
-        println(num1)
+        // Inverse point of P1
+        print("\nInversion of " + P1 + " = ")
+        P1.invert()
+        println(P1)
 
-        num2.invert()
-        println(num2)
+        // Inverse point of P2
+        print("Inversion of " + P2 + " = ")
+        P2.invert()
+        println(P2)
     }
     
 }
